afaec369f175fc0fae36c3421bbd025b1823a4e6
KateProxa <ekater_1997@mail.ru>
2023-12-20 02:08:58 +0300

    Add documentation

---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['scrollableframe_0',['ScrollableFrame',['../classscr_1_1_scrollable_frame.html',1,'scr']]]
+];
---
+++ docs/html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tooltip_0',['Tooltip',['../classscr_1_1_tooltip.html',1,'scr']]]
+];
